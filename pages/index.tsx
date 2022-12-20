37727ba3724145acfbe7caeb7bdef83eac3259b8
barrosdvlpr <barrosdvlpr@gmail.com>
2022-12-20 09:20:48 -0300

    write async functions with async await operators

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,60 +7,42 @@ export default function Home() {
   )
 }
 
-const example = new Promise<number>((resolve, reject) => {
-  // Use resolve or reject to determine the fate of the promise
-  resolve(123)
-  // reject(new Error('failed'))
-});
-
-example.then(value => {
-  console.log('then', value);
-});
-
-example.catch(error => {
-  console.log('catch', error);
-});
-
-const first = new Promise<number>((resolve, reject) => {
-  resolve(123);
-})
-const second =
-  first
-    .then(value => {
-      // Control the fate of second
-      // return 456
-      // return new Promise((res) => res(789));
-      // return new Promise((res, rej) => rej(new Error('example')));
-      // throw new Error('example');
-      foo.bar;
-    });
-
-// console.log(first === second); // false
-
-second.then(value => {
-  console.log('second then', value);
-});
-second.catch(error => {
-  console.log('second catch', error);
-});
+// async function foo() {
+//   return 123
+// }
+// // console.log(foo());
+// foo().then(value => {
+//   console.log(value);
+// });
 
 
+process.on('unhandledRejection', () => null);
 
-new Promise<number>((res, rej) => {
-  res(123);
-})
-  .then(res => {
-    console.log(res);
-    return 456;
-  })
-  .then(res => {
-    console.log(res);
-    return 789;
-  })
-  .then(res => {
-    console.log(res);
-    // foo;
-  })
-  .catch(err => {
-    console.log('ERROR:', err.message);
-  });
+const notAPromise =
+  123;
+const promiseThatWillResolve =
+  new Promise(res => res(456));
+const promiseThatWillReject =
+  new Promise((res, rej) => rej(new Error('Hello')));
+
+// async function foo() {
+//   const res1 = await notAPromise;
+//   console.log({ forNotAPromise: res1 });
+//   const res2 = await promiseThatWillResolve;
+//   console.log({ forPromiseThatWillResolve: res2 });
+//   try {
+//     const res3 = await promiseThatWillReject;
+//     console.log('I will never get called as error is thrown in previous line');
+//   }
+//   catch (e) {
+//     console.log({ forPromiseThatWillReject: e.message });
+//   }
+// }
+// foo();
+
+async function foo() {
+  console.log('Waiting 5 seconds');
+  await new Promise(res => setTimeout(res, 5000));
+  console.log('Done waiting!');
+}
+foo();
